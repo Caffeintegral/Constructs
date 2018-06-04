1b6c07ec27c1e2b60e961338ce80c1c51395f976
satsuki13579 <kazusaichinomiya2220@gmail.com>
2018-06-05 03:13:25 +0900

    スクレイピング始めました基盤完成

--- Web_scraping/wikiScraping.py
+++ Web_scraping/wikiScraping.py
@@ -1,13 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
 import codecs
+from urllib import parse
 
 
+names = ['Shimakaze', 'Akizuki']
 
-r = requests.get('http://wiki.wargaming.net/en/Ship:Shimakaze')
+urls = ['http://wiki.wargaming.net/en/Ship:', names[0]]
+url = ''.join(urls)
+r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 DetectRanges = soup.select('.t-performance_right')
 
-
+f = open('text.txt', 'w')
+list = []
 for DetectRange in DetectRanges:
-        print(DetectRange.text.strip().encode('cp932', "ignore"))
+        t = DetectRange.text.strip().encode('cp932', "ignore")
+        list.append(t)
+        
+        
+print(names[0])
+print("発見距離")
+f.write("発見距離")
+print(list[-2])
+f.write(list[-2].decode('cp932'))
+print("航空発見")
+f.write("航空発見")
+print(list[-1])
+f.write(list[-1].decode('cp932'))
+
+f.close()
+
+urls = ['http://wiki.wargaming.net/en/Ship:', names[1]]
+url = ''.join(urls)
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+DetectRanges = soup.select('.t-performance_right')
+
+f = open('text.txt', 'w')
+list = []
+for DetectRange in DetectRanges:
+        t = DetectRange.text.strip().encode('cp932', "ignore")
+        list.append(t)
+        
+        
+print(names[1])
+print("発見距離")
+f.write("発見距離")
+print(list[-2])
+f.write(list[-2].decode('cp932'))
+print("航空発見")
+f.write("航空発見")
+print(list[-1])
+f.write(list[-1].decode('cp932'))
+
+f.close()
+
+
